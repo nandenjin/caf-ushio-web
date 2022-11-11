58d6ab74ca6568a8f014168e5ea1ceefc7f06db7
Kazumi Inada <hello@nandenjin.com>
2022-11-11 12:35:05 +0900

    chore: Add basepath to CD workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath,
 }
 
 module.exports = nextConfig
